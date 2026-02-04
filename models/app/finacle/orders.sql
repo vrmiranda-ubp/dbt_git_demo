90a50d3dded7f4813f004125fd55978bdeb417e3
Vic Miranda <vic.miranda00@gmail.com>
2026-02-04 03:19:55 +0000

    -

---
+++ models/app/finacle/orders.sql
@@ -0,0 +1,22 @@
+with
+
+source as (
+
+    select * from {{ source('finacle', 'orders') }}
+
+),
+
+final as (
+    select O_ORDERKEY
+        , O_CUSTKEY
+        , O_ORDERSTATUS
+        , O_TOTALPRICE
+        , O_ORDERDATE
+        , O_ORDERPRIORITY
+        , O_CLERK
+        , O_SHIPPRIORITY
+        , O_COMMENT
+    from source
+)
+
+select * from final
