7ab10a06e9e455c38d19c1cf704691e75094eb7a
Vic Miranda <vic.miranda00@gmail.com>
2026-03-01 11:15:57 +0000

    -

---
+++ macros/generate_schema_name.sql
@@ -0,0 +1,18 @@
+{% macro generate_schema_name(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
+    {%- if target.name == 'dev' -%}
+
+        {%- if custom_schema_name is none -%}
+            {{ default_schema }}
+        {%- else -%}
+            {{ default_schema }}_{{ custom_schema_name | trim }}
+        {%- endif -%}
+
+    {%- else -%}
+
+        {{ custom_schema_name | trim }}
+
+    {%- endif -%}
+    
+{%- endmacro %}
